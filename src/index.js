e608b162281954972269b41c9e6ee7dd3b5558a8
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2020-04-29 19:20:57 +0300

    Render first component on page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { render } from "react-dom";
+import HopePage from "./components/HomePage.js";
+
+render(<HopePage />, document.getElementById("root"));
