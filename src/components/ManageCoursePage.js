6b8d72cbf71c8e15bef6c8093fd82e6639aaba04
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2020-05-02 15:09:30 +0300

    Introduce validation approach

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CourseForm from "./CourseForm";
 import * as courseApi from "../api/courseApi";
 import { toast } from "react-toastify";
 
 const ManageCoursePage = (props) => {
+  const [errors, setErrors] = useState({});
   const [course, setCourse] = useState({
     id: null,
     slug: "",
@@ -12,6 +13,13 @@ const ManageCoursePage = (props) => {
     category: "",
   });
 
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    if (slug) {
+      courseApi.getCourseBySlug(slug).then(setCourse);
+    }
+  }, [props.match.params.slug]);
+
   function handleChange({ target }) {
     setCourse({
       ...course,
@@ -19,8 +27,33 @@ const ManageCoursePage = (props) => {
     });
   }
 
+  function formIsValid() {
+    const _errors = {};
+
+    if (!course.title) {
+      _errors.title = "Title is required";
+    }
+
+    if (!course.authorId) {
+      _errors.authorId = "Author is required";
+    }
+
+    if (!course.category) {
+      _errors.category = "Category is required";
+    }
+
+    setErrors(_errors);
+
+    return Object.keys(_errors).length === 0;
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
+
+    if (!formIsValid()) {
+      return;
+    }
+
     courseApi.saveCourse(course).then(() => {
       props.history.push("/courses");
       toast.success("Course saved");
@@ -32,6 +65,7 @@ const ManageCoursePage = (props) => {
       <h2>Manage Course</h2>
       <CourseForm
         course={course}
+        errors={errors}
         onChange={handleChange}
         onSubmit={handleSubmit}
       />
